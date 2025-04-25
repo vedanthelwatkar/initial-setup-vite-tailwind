d36a50dd584e81a4ee8362809a67fdaeb8a06b23
Vedant Elwatkar <vedanthelwatkar@gmail.com>
2025-04-25 12:22:59 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="w-full h-[100dvh] flex items-center justify-center">
-      <div className="text-[#60279e]">Aceternity + Tailwind Initial Setup</div>
+      <div className="text-[#60279e]">Vite + Tailwind Initial Setup</div>
     </div>
   );
 };
