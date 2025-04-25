0a9dc772f26f8ab7257ef1b744d1acff22dd5a29
Vedant Elwatkar <vedanthelwatkar@gmail.com>
2025-04-25 11:33:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="w-full h-[100dvh] flex items-center justify-center">
+      <div className="text-[#60279e]">Aceternity + Tailwind Initial Setup</div>
+    </div>
+  );
+};
+
+export default App;
