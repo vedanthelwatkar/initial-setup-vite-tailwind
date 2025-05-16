3068e0b735f32b5b487a0edd49f3bdfcfaf22953
Vedant Elwatkar <vedanthelwatkar@gmail.com>
2025-05-16 11:54:49 +0530

    bg col changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="w-full h-[100dvh] flex items-center justify-center">
+    <div className="w-full h-dvh flex items-center justify-center">
       <div className="text-[#60279e]">Vite + Tailwind Initial Setup</div>
     </div>
   );
